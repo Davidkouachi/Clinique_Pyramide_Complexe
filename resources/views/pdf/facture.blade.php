9a523cdaacc31b9691f992bba8ebf0d87c08a221
DavidKouachi <Davidkouachi01@gmail.com>
2024-11-08 04:24:20 +0000

    Select2

--- resources/views/pdf/facture.blade.php
+++ resources/views/pdf/facture.blade.php
@@ -100,33 +100,45 @@
 
 <script>
     $(document).ready(function() {
+        $('.select2').select2({
+            tags: true,
+            theme: 'bootstrap',
+            placeholder: 'Selectionner',
+            language: {
+                noResults: function() {
+                    return "Aucun résultat trouvé";
+                }
+            },
+            width: '100%',
+        });
+
         // Vérifiez la version de jQuery
-        if (typeof jQuery !== 'undefined') {
-            console.log("jQuery version:", jQuery.fn.jquery);
-        } else {
-            console.error("jQuery n'est pas chargé.");
-        }
+        // if (typeof jQuery !== 'undefined') {
+        //     console.log("jQuery version:", jQuery.fn.jquery);
+        // } else {
+        //     console.error("jQuery n'est pas chargé.");
+        // }
 
         // Vérifiez la version de Select2
-        if ($.fn.select2) {
-            console.log("Select2 version:", $.fn.select2.defaults.version);
-            console.log($.fn.tooltip.Constructor.VERSION);
+        // if ($.fn.select2) {
+        //     console.log("Select2 version:", $.fn.select2.defaults.version);
+        //     console.log($.fn.tooltip.Constructor.VERSION);
 
-            $('.select2').select2({
-                tags: true,
-                theme: 'bootstrap',
-                placeholder: 'Selectionner',
-                language: {
-                    noResults: function() {
-                        return "Aucun résultat trouvé";  // Modifier le message ici
-                    }
-                },
-                width: 'resolve',
-            });
+        //     $('.select2').select2({
+        //         tags: true,
+        //         theme: 'bootstrap',
+        //         placeholder: 'Selectionner',
+        //         language: {
+        //             noResults: function() {
+        //                 return "Aucun résultat trouvé";  // Modifier le message ici
+        //             }
+        //         },
+        //         width: 'resolve',
+        //     });
 
-        } else {
-            console.error("Select2 n'est pas chargé correctement.");
-        }
+        // } else {
+        //     console.error("Select2 n'est pas chargé correctement.");
+        // }
     });
 </script>
 
